be90018ca8bf9361b5a0841488cfefddc0cfb5fc
Jared Grippe <jared@deadlyicon.com>
2018-11-10 09:10:35 -0800

    WIP

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ module.exports = async function(term){
   });
   const page = await browser.newPage();
   const url = `https://duckduckgo.com/?q=${encodeURIComponent(term)}`
-  await page.goto(url)
-  await page.waitForNavigation()
-  const node = await page.$('.js-spelling-suggestion-link')
+  try{
+    await page.goto(url, {timeout: 2000})
+  }catch(error){
+    console.error(`failed to goto url ${url}`)
+    throw error
+  }
+  try{
+    await page.waitForSelector('.js-spelling-suggestion-link', {timeout: 1000})
+  }catch(error){
+    console.error('failed waiting for result selector')
+    throw error
+  }
+  let node
+  try{
+    node = await page.$('.js-spelling-suggestion-link')
+  }catch(error){
+    console.error('failed looking for result on page')
+    throw error
+  }
   const result = await page.evaluate(node => node.innerText, node);
   await browser.close();
   return result
