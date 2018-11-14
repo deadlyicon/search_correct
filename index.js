a879e90dc2761c0f7d6121b50e3d47a01ffa5f6f
Jared Grippe <jared@deadlyicon.com>
2018-11-14 08:48:01 -0800

    WIP

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
 const debug = require('debug')('search_correct')
-const puppeteer = require('puppeteer')
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 module.exports = async function(term){
-  debug(`launching chrome`)
-  const browser = await puppeteer.launch({
-    ignoreHTTPSErrors: true,
-    headless: true,
-  });
-  try{
-    const page = await browser.newPage()
-    const result = await queryForResult(page, term)
-    return result
-  }catch(error){
-    throw error
-  }finally{
-    await browser.close()
-  }
-}
-
-const queryForResult = async function(page, term){
-  const url = `https://duckduckgo.com/?q=${encodeURIComponent(term)}`
-  try{
-    debug(`navigating to ${url}`)
-    await page.goto(url, {timeout: 5000})
-  }catch(error){
-    debug(`failed to goto url ${url}`)
-    throw error
-  }
-  try{
-    debug(`waiting for spelling selector`)
-    await page.waitForSelector('.js-spelling-suggestion-link', {timeout: 5000})
-  }catch(error){
-    debug('failed waiting for result selector', error)
-    return term
-  }
-  let node
-  try{
-    debug(`getting spelling selector node`)
-    node = await page.$('.js-spelling-suggestion-link', {timeout: 5000})
-  }catch(error){
-    debug('failed looking for result on page')
-    return term
-  }
-  const result = await page.evaluate(node => node.innerText, node);
-  debug(`found result: ${result}`)
-  return result
+  const url = `https://duckduckgo.com/html/?q=${encodeURIComponent(term)}`
+  debug(`url: ${url}`)
+  const response = await axios.get(url)
+  const $ = cheerio.load(response.data)
+  return $('#did_you_mean a').first().text() || term
 }
