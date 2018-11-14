31a34f00d09384c742b85d03a1b26a1029c0744e
Jared Grippe <jared@deadlyicon.com>
2018-11-14 08:51:13 -0800

    moved away from puppeteer and moved to google complete/search xml api

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 module.exports = async function(term){
-  const url = `https://duckduckgo.com/html/?q=${encodeURIComponent(term)}`
+  const url = `http://google.com/complete/search?output=toolbar&q=${encodeURIComponent(term)}`
   debug(`url: ${url}`)
   const response = await axios.get(url)
+  debug(`json: ${response.data}`)
   const $ = cheerio.load(response.data)
-  return $('#did_you_mean a').first().text() || term
+  return $('suggestion').attr('data') || term
 }
